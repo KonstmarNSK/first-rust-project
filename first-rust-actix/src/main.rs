d0cd1c77755700cd544bbafc664947d8cb42c73e
KonstmarNSK <konstmar@yandex.ru>
2019-09-26 07:54:26 +0600

    Added askama template

--- first-rust-actix/src/main.rs
+++ first-rust-actix/src/main.rs
@@ -1,16 +1,29 @@
+extern crate askama; 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use askama::Template;
+
+#[derive(Template)] 
+#[template(path = "hello.html")]               
+struct HelloTemplate<'a> { 
+    name: &'a str, 
+}
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+fn greeter(s : web::Path<String>) -> impl Responder{
+    HelloTemplate{name : s.as_ref()}.render().unwrap()
+}
+
 fn main() {
-    let ip_addr = std::env::args().nth(1).expect("no pattern given");
+    let ip_addr = std::env::args().nth(1).expect("Specify host. Example: first-rust-actix localhost:8080");
     println!("Will bind to {}", ip_addr);
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
+            .route("/{name}", web::get().to(greeter))
     })
     .bind(ip_addr)
     .unwrap()
